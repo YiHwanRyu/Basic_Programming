54c785cd42320abfd1f0e26982f8d18b119b50b6
YiHwanRyu <yihwandev@gmail.com>
2023-04-10 13:17:42 +0900

    [UPDATE] 나눗셈 연산 시 실수형으로 바꾸어 소수점 계산이 가능하도록 수정

--- C_사칙연산.cpp
+++ C_사칙연산.cpp
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main() {
 	int numberOne, numberTwo;
-	printf("두개의 정수를 입력하세요: ");
+	//글꼴변경 d2coding
+	printf("두 개의 정수를 입력하세요: ");
 	scanf("%d %d", &numberOne, &numberTwo);
 	printf("%d + %d = %d\n", numberOne, numberTwo, numberOne + numberTwo); 
 	printf("%d - %d = %d\n", numberOne, numberTwo, numberOne - numberTwo);
 	printf("%d * %d = %d\n", numberOne, numberTwo, numberOne * numberTwo);
-	printf("%d / %d = %d", numberOne, numberTwo, numberOne / numberTwo);
-
+	printf("%.f / %.f = %.1f", (float)numberOne, (float)numberTwo, (float)numberOne / (float)numberTwo);
+	
 	return 0;
-}
+} 
