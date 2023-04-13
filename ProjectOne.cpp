150254825f0cad24294787a68bec7e5963f1c900
YiHwanRyu <yihwandev@gmail.com>
2023-04-13 14:16:06 +0900

    [UPDATE] ProjectOne added function of age from Resident Registration Number

--- ProjectOne.cpp
+++ ProjectOne.cpp
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include<string>
+#include<time.h>
+#include<stdlib.h>
+#include<iostream>
 using namespace std;
 
 void printChoiceTicket() {
@@ -33,24 +37,58 @@ void printMoreChoiceTicket() {
 	printf("%s\n", "2. Exit");	 
 }
 
+int func_inputPersonNumber (string *inputPersonNumber) {
+	int age = 0;
+	string temp = *inputPersonNumber;
+	time_t timeNow;
+	struct tm* timeNowChange;
+	timeNow = time(NULL);
+	timeNowChange = localtime(&timeNow);
+	int year = timeNowChange -> tm_year + 1900;
+	int month = timeNowChange -> tm_mon + 1;
+	int day = timeNowChange -> tm_mday;
+	if (temp.substr(6, 1) == "3" || temp.substr(6, 1) == "4") {
+		age = 23 - atoi(temp.substr(0, 2).c_str());
+	} else {
+		string ageRaw = "";
+		ageRaw = "19" + temp.substr(0, 2);
+		age = 2023 - atoi(ageRaw.c_str());
+	}
+	
+	if (month < atoi(temp.substr(2, 2).c_str())) {
+		age--;
+	} else if(month == atoi(temp.substr(2, 2).c_str()))  {
+		if(day <= atoi(temp.substr(4, 2).c_str())) {
+			age--;
+		}
+	}
+	return age;
+}
 
 int main() {
 	int inputNumberChoiceTicket;
 	int inputMoreChoiceTicket;
-	int inputPersonNumber;
+	string inputPersonNumber = "";
 	int inputTicketNumbers;
 	int inputSpecialOffers;
+	int age;
+
 	do {	
 		while (true) { 
 			printChoiceTicket();
 			scanf("%d", &inputNumberChoiceTicket);
 			if (inputNumberChoiceTicket != 1 && inputNumberChoiceTicket != 2) {
-				printf("Error! Input again!\n\n");
+				printf("Ticket Type Error! Input again!\n\n");
 				continue;
 			} else {
 				// input other conditions & Errors(not yet)
 				printInputPersonNumber(); // 13 spaces and string format
-				scanf("%d", &inputPersonNumber);
+				cin >> inputPersonNumber;
+				if (inputPersonNumber.size() != 13) {
+					printf("Resident Registration Number Error! Input again!\n\n");
+					continue;
+				}	
+				age = func_inputPersonNumber(&inputPersonNumber);
 				printInputTicketNumbers();// more than 10 tickets or None error
 				scanf("%d", &inputTicketNumbers);
 				printSpecialOffers(); 
