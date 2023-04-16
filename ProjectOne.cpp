cc6588126aa7626d2f91028e6d1b6e70ae3347d5
YiHwanRyu <yihwandev@gmail.com>
2023-04-16 14:27:29 +0900

    [UPDATE] ProjectOne : main 함수의 입력을 inputData 함수를 통해 입력받도록 수정

--- ProjectOne.cpp
+++ ProjectOne.cpp
@@ -4,9 +4,21 @@
 #include<stdlib.h>
 #include<iostream>
 using namespace std;
+const int BABY_PRICE = 0,
+		 ADULT_DAY_PRICE = 56000, ADULT_NIGHT_PRICE = 46000,
+		 TEEN_DAY_PRICE = 47000, TEEN_NIGHT_PRICE = 40000,
+		 CHILD_DAY_PRICE = 44000, CHILD_NIGHT_PRICE = 37000,
+		 OLD_DAY_PRICE = 44000, OLD_NIGHT_PRICE = 37000;
+const int MIN_BABY = 2, 
+		 MIN_CHILD = 3, MIN_TEEN = 13, MIN_ADULT = 19,
+		 MAX_CHILD = 12, MAX_TEEN = 18, MAX_ADULT = 64;
+const int BABY = 1, CHILD = 2, TEEN = 3, ADULT = 4, OLD = 5;
+const float  DIFFICULTY_DISCOUNT_RATE = 0.6, MERIT_DISCOUNT_RATE = 0.5,
+			MULTIPLE_KIDS_DISCOUNT_RATE = 0.8, PREGNANT_DISCOUNT_RATE = 0.85;
+const int MAX_TICKET_COUNT = 10, MIN_TICKET_COUNT = 1;
 
 void printChoiceTicket() {
-	printf("%s\n", "Welcome to PolyLand!!");
+	printf("\n%s\n", "Welcome to PolyLand!!");
 	printf("%s\n", "Choose your type of ticket!");
 	printf("%s\n", "1. Daytime");
 	printf("%s\n", "2. Nighttime");	 
@@ -64,43 +76,63 @@ int func_inputPersonNumber (string *inputPersonNumber) {
 	}
 	return age;
 }
+int inputNumberChoiceTicket(){
+	int number;
+	printChoiceTicket();
+	scanf("%d", &number);
+	return number;
+}
+string inputPersonNumber(){
+	string number;
+	printInputPersonNumber();
+	cin >> number;
+	return number;
+}
+int inputTicketNumbers(){
+	int number;
+	printInputTicketNumbers();
+	scanf("%d", &number);
+	return number;
+}
+int inputSpecialOffers(){
+	int number;
+	printSpecialOffers(); 
+	scanf("%d", &number);
+	return number;
+}
+void inputData(int *numberChoiceTicket, string *personNumber, 
+			   int *ticketNumbers, int *specialOffers) {
+	*numberChoiceTicket = inputNumberChoiceTicket();
+	*personNumber = inputPersonNumber();
+	*ticketNumbers = inputTicketNumbers();
+	*specialOffers = inputSpecialOffers();
+		   	
+}
+
 
 int main() {
-	int inputNumberChoiceTicket;
-	int inputMoreChoiceTicket;
-	string inputPersonNumber = "";
-	int inputTicketNumbers;
-	int inputSpecialOffers;
+	int numberChoiceTicket;
+	string personNumber = "";
+	int ticketNumbers;
+	int specialOffers;
+	int moreChoiceTicket;
+	int isExit;
 	int age;
 
 	do {	
 		while (true) { 
-			printChoiceTicket();
-			scanf("%d", &inputNumberChoiceTicket);
-			if (inputNumberChoiceTicket != 1 && inputNumberChoiceTicket != 2) {
-				printf("Ticket Type Error! Input again!\n\n");
-				continue;
-			} else {
-				// input other conditions & Errors(not yet)
-				printInputPersonNumber(); // 13 spaces and string format
-				cin >> inputPersonNumber;
-				if (inputPersonNumber.size() != 13) {
-					printf("Resident Registration Number Error! Input again!\n\n");
-					continue;
-				}	
-				age = func_inputPersonNumber(&inputPersonNumber);
-				printInputTicketNumbers();// more than 10 tickets or None error
-				scanf("%d", &inputTicketNumbers);
-				printSpecialOffers(); 
-				scanf("%d", &inputSpecialOffers);
-				// print result place
-				
+			inputData(&numberChoiceTicket, &personNumber, &ticketNumbers, &specialOffers);
+			//cout << numberChoiceTicket << " : " << personNumber << " : " << ticketNumbers << " : " << specialOffers << "\n";
+			printMoreChoiceTicket();
+			scanf("%d" , &moreChoiceTicket);
+			if (moreChoiceTicket == 2) {
 				break;
-			}	
+			}		
 		}
-		printMoreChoiceTicket();
-		scanf("%d", &inputMoreChoiceTicket);		
-	} while (inputMoreChoiceTicket == 1);
+		printf("Continue? (1: New Order, 2: Exit Program) : ");
+		scanf("%d", &isExit);	
+				
+	} while (isExit == 1);
 	
 	return 0;
 }
