8ce337cc7287fa658e4125361f7896e44e891bda
YiHwanRyu <yihwandev@gmail.com>
2023-04-18 10:03:22 +0900

    [ADD] ProjectKiosk : PrintForInputs Class

---
+++ ProjectKiosk/src/PrintForInputs.java
@@ -0,0 +1,41 @@
+
+
+public class PrintForInputs {
+
+	void printChoiceTicket() {
+		System.out.println("Welcome to PolyLand!!");
+		System.out.println("Choose your type of ticket!");
+		System.out.println("1. Daytime");
+		System.out.println("2. Nighttime");
+	}
+
+	void printInputPersonNumber() {
+		System.out.println("Please input your Resident Registration Number!");
+		System.out.println("**Only input Numbers**");
+	}
+
+	void printInputTicketNumbers() {
+		System.out.println("How many tickets do you want?");
+		System.out.println("**Up to 10**");
+	}
+
+	void printSpecialOffers() {
+		System.out.println("Choose your special offer!");
+		System.out.println("1. None (Special offer for age is already applied)");
+		System.out.println("2. Person With Difficulty");
+		System.out.println("3. Men of national merit");
+		System.out.println("4. Multiple kids (More than two)");
+		System.out.println("5. Pregnant Woman");
+	}
+
+	void printMoreChoiceTicket() {
+		System.out.println("Continue to buy tickets or Exit?");
+		System.out.println("1. Buying ticket");
+		System.out.println("2. Exit");
+	}
+	
+	void printIsExit() {
+		System.out.println("Continue? (1: New Order, 2: Exit Program) : ");
+	}
+
+}
