222c6a890127684b474d8cd5ebf3a532d30b4635
YiHwanRyu <yihwandev@gmail.com>
2023-04-18 10:02:44 +0900

    [UPDATE] ProjectKiosk : MainKiosk에 Main logic 추가

--- ProjectKiosk/src/MainKiosk.java
+++ ProjectKiosk/src/MainKiosk.java
@@ -3,7 +3,26 @@ public class MainKiosk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Variables
+		int moreChoiceTicket;
+		int isExit;
+		
+		// Object for this class
+		Inputs inputClass = new Inputs();
+		
+		// Main logic
+		do {	
+			while (true) { 
+				inputClass.inputData();
+				
+				moreChoiceTicket = inputClass.inputMoreChoiceTicket();
+				if (moreChoiceTicket == 2) {
+					break;
+				}		
+			}
+			isExit = inputClass.inputIsExit();
+		} while (isExit == 1);
+		
 	}
 
 }
