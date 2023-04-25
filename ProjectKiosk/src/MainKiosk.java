dde82a4c4726464a8874d9aae02e3ad5dc33fa01
YiHwanRyu <yihwandev@gmail.com>
2023-04-25 17:16:19 +0900

    [ADD] ProjectKiosk : RunKiosk Class를 생성하여 MainKiosk Class의 가독성을 높임

--- ProjectKiosk/src/MainKiosk.java
+++ ProjectKiosk/src/MainKiosk.java
@@ -5,47 +5,16 @@ public class MainKiosk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Variables
-		int moneyOfRow;
-		int moneyOfOrder = 0;
-		int moreChoiceTicket;
-		int isExit;
 		// Objects for Main class
-		List<OrderList> data = new ArrayList<OrderList>();
 		List<OrderList> dataTotal = new ArrayList<OrderList>();
 		Inputs inputClass = new Inputs();
-		Calculations calculationsClass = new Calculations();
 		Outputs outputs = new Outputs();
-		SaveOrders saveOrders = new SaveOrders();
+		RunKiosk runKiosk = new RunKiosk();
 		// Main logic
 		do {	
-			while (true) { 
-				//Inputs
-				inputClass.inputData();
-				
-				//Calculations
-				int age = calculationsClass.calculationOfAge(inputClass.getPersonNumber());
-				moneyOfRow = calculationsClass.calculation(inputClass.getPersonNumber(), inputClass.getNumberChoiceTicket(), 
-														   inputClass.getTicketNumbers(), inputClass.getSpecialOffers(), age);
-				moneyOfOrder += moneyOfRow;
-				
-				//Save and outputs of row
-				outputs.printResultOfRow(moneyOfRow);
-				data = saveOrders.saveOrdersData(data, inputClass.getNumberChoiceTicket(), age, inputClass.getTicketNumbers(), 
-												moneyOfRow, inputClass.getSpecialOffers());
-				moreChoiceTicket = inputClass.inputMoreChoiceTicket();
-				if (moreChoiceTicket == 2) {
-					outputs.printResultOfOrder(data, moneyOfOrder);
-					dataTotal.addAll(data);
-					break;
-				}		
-			}
-			isExit = inputClass.inputIsExit();
-			//Initialization of data
-			data = new ArrayList<OrderList>();
-			moneyOfOrder = 0;
-
-		} while (isExit == 1);
+			dataTotal = runKiosk.runOrder();	
+		} while (inputClass.inputIsExit() == Constants.NEW_ORDER);
+		
 		//Total outputs
 		outputs.printEndOfProgram(dataTotal);
 	}
---
+++ ProjectKiosk/src/RunKiosk.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunKiosk {
+	// Variables
+	private int moneyOfRow;
+	private int moneyOfOrder;
+	// Objects for RunKiosk class
+	private List<OrderList> data = null;
+	private List<OrderList> dataTotal = null;
+	private Inputs inputClass = null;
+	private Calculations calculationsClass = null;
+	private Outputs outputClass = null;
+	private SaveOrders saveOrderClass = null;
+	// Constructor
+	RunKiosk() {
+		data = new ArrayList<OrderList>();
+		dataTotal = new ArrayList<OrderList>();
+		inputClass = new Inputs();
+		calculationsClass = new Calculations();
+		outputClass = new Outputs();
+		saveOrderClass = new SaveOrders();
+	}
+	// runOrder
+	List<OrderList> runOrder() {
+		while (true) { 
+			//Inputs
+			inputClass.inputData();
+						
+			//Calculations
+			moneyOfRow = calculationsClass.calculation(inputClass);
+			moneyOfOrder += moneyOfRow;
+						
+			//Save and outputs of row
+			data = saveOrderClass.saveOrdersData(data, moneyOfRow, inputClass);
+						
+			//Save data and Question about another order
+			if (inputClass.inputMoreChoiceTicket() == Constants.NO) {
+				dataTotal.addAll(outputClass.printResultOfOrder(data, moneyOfOrder));
+				break;
+			}		
+		}
+		//Initialization of data
+		data = new ArrayList<OrderList>();
+		moneyOfOrder = 0;	
+		return dataTotal;
+	}
+}
