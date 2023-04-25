7a0a534dfc3dc43a6b2b05e516a5011eaa14d024
YiHwanRyu <yihwandev@gmail.com>
2023-04-25 17:10:59 +0900

    [UPDATE] ProjectKiosk : Inputs Class에서 추가주문과 종료를 입력받는 메소드의 입력부를 잘못된 입력이 들어왔을 때, 다시 입력받도록 수정

--- ProjectKiosk/src/Inputs.java
+++ ProjectKiosk/src/Inputs.java
@@ -60,14 +60,20 @@ public class Inputs {
 	}
 	
 	int inputMoreChoiceTicket(){
-		printForInputs.printMoreChoiceTicket();
-		int number = scanner.nextInt();
+		int number;
+		do {
+			printForInputs.printMoreChoiceTicket();
+		number = scanner.nextInt();
+		} while (number != 1 && number != 2);
 		return number;
 	}
 	
 	int inputIsExit(){
-		printForInputs.printIsExit();
-		int number = scanner.nextInt();
+		int number;
+		do {
+			printForInputs.printIsExit();
+		number = scanner.nextInt();
+		} while (number != 1 && number != 2);
 		return number;
 	}
 	
